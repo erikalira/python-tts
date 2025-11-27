abd91d50070340875c753b37dc985aad919d9460
Erika Lira <liraerika00@gmail.com>
2025-11-27 02:38:37 -0300

    chore: Bump version to 0.2.4

--- src/__version__.py
+++ src/__version__.py
@@ -1,6 +1,6 @@
 """Version information for TTS Hotkey Windows."""
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = "Erika Lira"
 __description__ = "Discord TTS Bot with hotkey support for Windows"
 
